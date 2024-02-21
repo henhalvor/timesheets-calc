94f5b6e9606a7aa373f760f51d47436cb71f4c0b
henhal <henhalvor@gmail.com>
2024-02-21 20:09:45 +0100

    style image page

--- src/app/(protected)/timesheets/[slug]/page.tsx
+++ src/app/(protected)/timesheets/[slug]/page.tsx
@@ -1,7 +1,4 @@
-import {
-  getImageById,
-  getImageByWeekNumber,
-} from "@/lib/userImages";
+import { getImageById, getImageByWeekNumber } from "@/lib/userImages";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
@@ -16,20 +13,30 @@ export default async function Page({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <div>
-      <Image
-        className="h-auto w-60"
-        src={image.imageUrl}
-        alt="Image of timesheet"
-        width={600}
-        height={1000}
-      />
+    <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
+      <div className="border-solid border-gray-500 border-[1px] rounded-md p-1">
+        <Image
+          className="h-auto w-60"
+          src={image.imageUrl}
+          alt="Image of timesheet"
+          width={600}
+          height={1000}
+        />
+      </div>
       <div>
-        
         <form action={deleteImageById}>
-          <label >Image Id</label>
-          <input type="text" id="id" name="id" value={image.id} readOnly />
-          <button>Delete Image</button>
+          <label className="hidden">Image Id</label>
+          <input
+            type="text"
+            id="id"
+            name="id"
+            value={image.id}
+            readOnly
+            className="hidden"
+          />
+          <button className="border-solid border-gray-500 border-[1px] rounded-md p-2 hover:border-black hover:font-semibold hover:scale-105 m-2">
+            Delete Image
+          </button>
         </form>
       </div>
     </div>
