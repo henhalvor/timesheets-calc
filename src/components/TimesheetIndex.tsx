ac9c2f9a9b2089fea4a1aee46839b0fea219105d
henhal <henhalvor@gmail.com>
2024-02-25 19:34:38 +0100

    fix text color for production

--- src/components/TimesheetIndex.tsx
+++ src/components/TimesheetIndex.tsx
@@ -15,17 +15,17 @@ export default function TimesheetIndex({ timesheet }: TimesheetIndexProps) {
   return (
     <div className="flex w-[310px] md:flex-1 md:w-auto flex-col justify-between items-center border-solid border-gray-500 border-[1px] rounded-md p-4 relative gap-10">
       <div className="flex flex-col gap-3">
-        <h2 className="text-2xl font-bold self-center mb-8">
+        <h2 className="text-2xl font-bold self-center mb-8 text-black">
           Week {timesheet.weekNumber}
         </h2>
         <div className="flex flex-wrap justify-center gap-8">
-          <div className="flex flex-col gap-3 *:underline *:underline-offset-4">
+          <div className="flex flex-col gap-3 *:underline *:underline-offset-4 *:text-black">
             <p>Hours: <span className="font-semibold">{timesheet.regularHours}</span></p>
             <p>Accrued Hours: <span className="font-semibold">{timesheet.accruedHours}</span></p>
             <p>Used Accrued Hours: <span className="font-semibold">{timesheet.usedAccruedHours}</span></p>
             <p>Overtime Hours: <span className="font-semibold">{timesheet.overtimeHours}</span></p>
           </div>
-          <div className="flex flex-col gap-3 *:underline *:underline-offset-4">
+          <div className="flex flex-col gap-3 *:underline *:underline-offset-4 *:text-black">
             <p>Travel Distance: <span className="font-semibold">{timesheet.travelDistanceKM} KM</span></p>
             <p>Tool Comp: <span className="font-semibold">{timesheet.extraToolCompensation} KR</span></p>
             <p>Transport Comp: <span className="font-semibold">{timesheet.extraTransportationCompensation} KR</span></p>
@@ -35,13 +35,13 @@ export default function TimesheetIndex({ timesheet }: TimesheetIndexProps) {
       <div className="flex flex-col sm:flex-row gap-12 mb-4 items-center">
         <div className="flex flex-col gap-3">
           <a
-            className="block italic text-2xl font-medium underline underline-offset-4 mb-4 self-center hover:scale-105"
+            className="text-black block italic text-2xl font-medium underline underline-offset-4 mb-4 self-center hover:scale-105"
             href={timesheet.imageUrl}
           >
             Open
           </a>
           <a
-            className="block italic text-2xl font-medium underline underline-offset-4 self-center hover:scale-105"
+            className="text-black block italic text-2xl font-medium underline underline-offset-4 self-center hover:scale-105"
             href={timesheet.imageDownloadUrl}
           >
             Download
@@ -62,7 +62,7 @@ export default function TimesheetIndex({ timesheet }: TimesheetIndexProps) {
           </button>
         </form>
       </div>
-      <p className="text-xs absolute bottom-0 right-2">
+      <p className="text-black text-xs absolute bottom-0 right-2">
           Uploaded:{" "}
           {uploadDate.toLocaleDateString("en-GB", {
             day: "2-digit",
