d529693304f83e64cf3995710ffea5644d451fbc
henhal <henhalvor@gmail.com>
2024-03-04 19:18:13 +0100

    moved getListedYears() to utils and added argument of "numberOfYears"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,14 @@ export function cn(...inputs: ClassValue[]) {
 export function multiplyArrayElements(arr: number[]) {
   return arr.reduce((acc, curr) => acc + curr, 0);
 }
+
+
+
+export function getListedYears(numberOfYears: number) {
+  const currentYear = new Date().getFullYear();
+  const years = [];
+  for (let i = currentYear; i >= currentYear - (numberOfYears - 1); i--) {
+    years.push(i);
+  }
+  return years;
+}
